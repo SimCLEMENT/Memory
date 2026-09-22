189511cfa722818c9c2691aa68050e079ff487e5
Simon CLEMENT <comptepoursimon@gmail.com>
2026-09-22 08:54:44 +0200

    feat: core array data logic and fisher-yates shuffle functional

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const dimension = 150;
+const imgStart = Math.floor(Math.random() * 100) + 1;
+
+let images = [];
+for (i = 0 ; i < 8 ; i++) {
+    images.push(`https://picsum.photos/id/${imgStart + i}/${dimension}/${dimension}`);
+}
+
+let cards = [...images, ...images];
+
+function shuffle(array) {
+    for(let i = array.length -1 ; i > 0 ; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+console.log(images);
+console.log(cards);
+console.log(shuffle(cards));
